c7220eda658d9a65767757170fa21dc5e616c735
Stefanotr <180202289+Stefanotr@users.noreply.github.com>
2026-03-03 14:06:30 +0100

    feat: initialisation du modèle avec la classe Prestation
    - Création de la classe Prestation avec encapsulation (attributs privés)
    - Ajout du constructeur, des getters et de la méthode toString

---
+++ Abonnements salle de sport/src/Prestation.java
@@ -0,0 +1,27 @@
+public class Prestation {
+    private String code;
+    private String libelle;
+    private String prix;
+
+    public Prestation(String code, String libelle, String prix) {
+        this.code=code;
+        this.libelle=libelle;
+        this.prix=prix;
+    }
+    public String getCode() {
+        return code;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    @Override
+    public String toString() {
+        return "Prestation [code=" + code + ", libelle=" + libelle + ", prix= " + prix + "€]";
+    }
+}
